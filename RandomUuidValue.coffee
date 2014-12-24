587be027a43b701b69db5480acb4c3bc068497f4
Hunter Skrasek <hunterskrasek@me.com>
2014-12-24 12:19:03 -0600

    Not too familiar with CoffeeScript, needed to fix some errors, and now the extension works again
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

--- RandomUuidValue.coffee
+++ RandomUuidValue.coffee
@@ -1,13 +1,14 @@
 RandomUuidValue = ->
-@evaluate = (context) ->
-  "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace /[xy]/g, (c) ->
-    r = Math.random() * 16 | 0
-    v = (if c is "x" then r else (r & 0x3 | 0x8))
-    v.toString 16
+    @evaluate = (context) ->
+      "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace /[xy]/g, (c) ->
+        r = Math.random() * 16 | 0
+        v = (if c is "x" then r else (r & 0x3 | 0x8))
+        v.toString 16
 
+    @title = ->
+      "UUID"
 
-@title = ->
-  "UUID"
+    return
 
 RandomUuidValue.identifier = "com.hskrasek.PawExtensions.RandomUuidValue"
 RandomUuidValue.title = "Generate Random UUID"
--- RandomUuidValue.js
+++ RandomUuidValue.js
@@ -2,19 +2,18 @@
 (function() {
   var RandomUuidValue;
 
-  RandomUuidValue = function() {};
-
-  this.evaluate = function(context) {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
-      var r, v;
-      r = Math.random() * 16 | 0;
-      v = (c === "x" ? r : r & 0x3 | 0x8);
-      return v.toString(16);
-    });
-  };
-
-  this.title = function() {
-    return "UUID";
+  RandomUuidValue = function() {
+    this.evaluate = function(context) {
+      return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+        var r, v;
+        r = Math.random() * 16 | 0;
+        v = (c === "x" ? r : r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    };
+    this.title = function() {
+      return "UUID";
+    };
   };
 
   RandomUuidValue.identifier = "com.hskrasek.PawExtensions.RandomUuidValue";
